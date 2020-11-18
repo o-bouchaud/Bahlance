5c79db1718b7d9063fd13c8d96d8425bb9813f39
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-11-18 11:15:35 +0100

    InkTestingScript Script created;

---
+++ Assets/Scripts/inkTestingScript.cs
@@ -0,0 +1,23 @@
+﻿using Ink.Runtime;
+//To be able to access Ink's class in the script;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inkTestingScript : MonoBehaviour
+{
+
+    public TextAsset inkJSON;
+    private Story story;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
