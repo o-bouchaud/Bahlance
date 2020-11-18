e6e93a085b3cd64a68165b50328b0671f1537b9e
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-11-18 15:16:17 +0100

    InkTestingScript updated;
    Text String Variable initialised;
    If statement added;
    => Story goes on till a choice has to be made;

--- Assets/Scripts/inkTestingScript.cs
+++ Assets/Scripts/inkTestingScript.cs
@@ -15,7 +15,7 @@ public class inkTestingScript : MonoBehaviour
         story = new Story(inkJSON.text);
         //we create a new story object using Ink's Story class;
         //we give it inkJSON's text as a parameter;
-        loadStoryChunk();
+        Debug.Log(loadStoryChunk());
         //we're calling the loadStoryChunk function;
 
         //Continue loads the story each line at a time;
@@ -28,11 +28,22 @@ public class inkTestingScript : MonoBehaviour
         
     }
 
-    void loadStoryChunk()
+    string loadStoryChunk()
+    //the function will return some text so it's a string function and not void;
     {
+        string text = "";
+        //we create a string variable and leave it empty;
+
+        if(story.canContinue)
+        //we create an if statement that checks if the canContinue boolean is true;
+        //canContinue is an Ink boolean, we can use it because we called "using Ink.Runtime;"
+        {
+            text = story.ContinueMaximally();
+            //Continue loads the story each line at a time;
+         //ContinueMaximally loads the story to the next choice
+        }
+
+        return text;
         
-        story.ContinueMaximally();
-        //Continue loads the story each line at a time;
-        //ContinueMaximally loads the story to the next choice
     }
 }
